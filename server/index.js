8cd62354c0dc90bbd4ebb6d51d2334fa86d7a76d
Daryll Ricalde <rstevendaryll@gmail.com>
2022-01-03 00:38:39 +1100

    final commit 3/1/22 AU

--- server/index.js
+++ server/index.js
@@ -1,4 +1,8 @@
+// index.js SERVER-SIDE
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();      // allows us to use methods in this Express instance
+
